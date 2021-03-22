6ba7b2b13932b9ed4b68150b3dbff9c26604fb01
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-21 22:37:42 -0700

    Feat: Added permission checks and button click listener
    The clicks will be used to handle permission requests from the user.

--- app/src/main/java/com/anomalydev/trackyourdistance/PermissionFragment.kt
+++ app/src/main/java/com/anomalydev/trackyourdistance/PermissionFragment.kt
@@ -5,15 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.anomalydev.trackyourdistance.Permissions.hasLocationPermission
+import com.anomalydev.trackyourdistance.Permissions.requestLocationPermission
+import com.anomalydev.trackyourdistance.databinding.FragmentPermissionBinding
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 
-class PermissionFragment : Fragment() {
+class PermissionFragment : Fragment(), EasyPermissions.PermissionCallbacks {
+
+    private var _binding: FragmentPermissionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_permission, container, false)
+
+        _binding = FragmentPermissionBinding.inflate(
+                inflater,
+                container,
+                false
+        )
+
+        binding.continueButton.setOnClickListener {
+            if (hasLocationPermission(requireContext())) {
+                findNavController().navigate(R.id.action_permissionFragment_to_mapsFragment)
+            } else {
+                requestLocationPermission(this)
+            }
+        }
+
+        return binding.root
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        findNavController().navigate(R.id.action_permissionFragment_to_mapsFragment)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionDenied(this, perms[0])) {
+            AppSettingsDialog.Builder(requireActivity()).build().show()
+        } else {
+            requestLocationPermission(this)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
