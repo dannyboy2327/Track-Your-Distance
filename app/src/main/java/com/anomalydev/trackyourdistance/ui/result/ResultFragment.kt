25a96fa8a7de51f609c11e1ae9852be2530a5a1a
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-25 14:48:01 -0700

    Feat: Extended BottomSheetDialogFragment()

--- app/src/main/java/com/anomalydev/trackyourdistance/ui/result/ResultFragment.kt
+++ app/src/main/java/com/anomalydev/trackyourdistance/ui/result/ResultFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.anomalydev.trackyourdistance.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class ResultFragment : Fragment() {
+class ResultFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
     }
 
