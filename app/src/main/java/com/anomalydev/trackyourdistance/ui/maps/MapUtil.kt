a566d0d84cb65676217c05171405f381cb04217b
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-24 19:53:39 -0700

    Feat: Added a util class for maps
    This object will return a camera position from setCameraPosition()

---
+++ app/src/main/java/com/anomalydev/trackyourdistance/ui/maps/MapUtil.kt
@@ -0,0 +1,14 @@
+package com.anomalydev.trackyourdistance.ui.maps
+
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+
+object MapUtil {
+
+    fun setCameraPosition(location: LatLng) : CameraPosition {
+        return CameraPosition.Builder()
+            .target(location)
+            .zoom(18f)
+            .build()
+    }
+}
