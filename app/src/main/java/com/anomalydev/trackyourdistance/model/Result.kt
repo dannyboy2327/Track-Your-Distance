f7f3186a5b3d54fe4becd0f4b5ae9b57375aad53
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-03-25 17:03:03 -0700

    Feat: Added a result data class
    This class will hold the distance and time.

---
+++ app/src/main/java/com/anomalydev/trackyourdistance/model/Result.kt
@@ -0,0 +1,9 @@
+package com.anomalydev.trackyourdistance.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Result(
+    var distance: String,
+    var time: String) : Parcelable
